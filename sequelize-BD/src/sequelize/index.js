8c99aaaa3f3d7c62150b78c7e6ea7c992971f417
caroalo <carolina.aloisio@gmail.com>
2024-11-07 20:24:08 -0300

    corregi mi problema con el Back

--- sequelize-BD/src/sequelize/index.js
+++ sequelize-BD/src/sequelize/index.js
@@ -1,35 +1,27 @@
 const { Sequelize } = require('sequelize');
 const { applyExtraSetup } = require('./extra-setup');
 
-// In a real app, you should keep the database connection URL as an environment variable.
-// But for this example, we will just use a local SQLite database.
-// const sequelize = new Sequelize(process.env.DB_CONNECTION_URL);
 const sequelize = new Sequelize({
-	dialect: 'sqlite',
-	storage: 'db.sqlite',
-	logQueryParameters: true,
-	benchmark: true
+  dialect: 'sqlite',
+  storage: 'db.sqlite',
+  logQueryParameters: true,
+  benchmark: true
 });
 
 const modelDefiners = [
-	require('./models/profesional.model'),
-	require('./models/cliente.model'),
-	require('./models/turno.model'),
-	require('./models/servicio.model'),
-	require('./models/evento.model'),
-	require('./models/horario.model'),
-	require('./models/confirmacion.model'),
-	// Add more models here...
-	// require('./models/item'),
+  require('./models/profesional.model'),
+  require('./models/cliente.model'),
+  require('./models/turno.model'),
+  require('./models/servicio.model'),
+  require('./models/evento.model'),
+  require('./models/horario.model'),
+  require('./models/confirmacion.model'),
 ];
 
-// We define all models according to their files.
 for (const modelDefiner of modelDefiners) {
-	modelDefiner(sequelize);
+  modelDefiner(sequelize);
 }
 
-// We execute any extra setup after the models are defined, such as adding associations.
 applyExtraSetup(sequelize);
 
-// We export the sequelize connection instance to be used around our app.
 module.exports = sequelize;
--- sequelize-BD/src/sequelize/models/confirmacion.model.js
+++ sequelize-BD/src/sequelize/models/confirmacion.model.js
@@ -1,20 +1,21 @@
-module.exports = (sequelize, DataTypes) => {
-    const Confirmacion = sequelize.define('confirmacion', {
-      codigo: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      fecha: {
-        type: DataTypes.DATEONLY,
-        allowNull: false
-      },
-      horario: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-    });
-  
-    return Confirmacion;
-  };
-  
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Confirmacion = sequelize.define('confirmacion', {
+    codigo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    fecha: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    horario: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return Confirmacion;
+};
